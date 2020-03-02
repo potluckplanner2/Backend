b06a8192064153492b4aa8bdd31043ece9fff93d
skooger <adamjskooglambda@gmail.com>
2020-03-02 11:13:28 -0800

    initial server

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+//const authRouter =  require('./auth/authRouter');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+//server.use('api/auth', authRouter);
+
+server.get('/', (req,res) => res.status(200).json({message: 'API is Running'}));
+
+module.exports = server;
